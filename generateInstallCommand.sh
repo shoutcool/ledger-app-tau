e41e676b458e9ff925e08f5ecb061ee8c292541e
shoutcool <shoutcool@gmail.com>
2021-04-02 18:17:21 +0200

    fixed installation command script

--- generateInstallCommand.sh
+++ generateInstallCommand.sh
@@ -5,15 +5,16 @@ dataSize=$((0x`cat debug/app.map |grep _envram_data | tr -s ' ' | cut -f2 -d' '|
 iconHex=$(python3 ~/ledger/nanos-secure-sdk/icon3.py --hexbitmaponly lamden.gif  2>/dev/null)
 installCmd=$(printf 'python -m ledgerblue.loadApp \
     --appFlags 0x40 \
-    --path "44'/789'" \
+    --path "44\x27/789\x27" \
     --curve ed25519 \
     --tlv \
     --targetId 0x31100004 \
+    --targetVersion="1.6.1" \
     --delete \
     --fileName bin/app.hex \
     --appName Lamden \
     --appVersion %s \
-    --dataSize %s\
+    --dataSize %s \
     --icon %s' "$appVersion" "$dataSize" "$iconHex")
 
 echo "$installCmd"
